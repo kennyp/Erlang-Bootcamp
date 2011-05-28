cfc473ea0d25410077216580486631475598977a
Kenny Parnell <k.parnell@gmail.com>
2011-05-28 14:48:48 -0400

    complete lab 3

---
+++ lab3/src/cache_sup.erl
@@ -0,0 +1,30 @@
+
+-module(cache_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    Cache = {cache, {cache, start_link, []},
+        permanent, 2000, worker, [cache]},
+    {ok, {{one_for_one, 10, 60}, [Cache]}}.
+
